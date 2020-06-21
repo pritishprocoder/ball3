96e23583ad839f420853425655e0c53dde9609b4
pritishprocoder <65237674+pritishprocoder@users.noreply.github.com>
2020-06-21 19:40:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,28 +3,31 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var ball;
+var ball,ballimg;
 var rectangle1,rectangle2,rectangle3;
+var slingshot;
 
 function preload()
 {
-	
+	ballimg=loadImage("paper.png");
 	
 }
 
 function setup() {
 	createCanvas(800, 700);
    ball= createSprite(30,600,20,20);
-  
- 
+   ball.addImage(ballimg);
+   ball.scale=0.3;
    rectangle1= createSprite(550,650,10,100);
    rectangle2=createSprite(600,700,100,10);
    rectangle3=createSprite(650,650,10,100);
 	engine = Engine.create();
 	world = engine.world;
-	
+	rectangle1.shapeColor=color(0,141,0);
+	rectangle2.shapeColor=color(0,141,0);
+	rectangle3.shapeColor=color(0,141,0);
 
-	
+	slingShot = new Slingshot(ball.body,{x:200,y:50});
 	
 
 	//Create a Ground
@@ -38,12 +41,13 @@ function setup() {
 
 
 function draw() {
-	
+	mouseDragged();
+	mouseReleased();
   rectMode(CENTER);
 
  ball.x=mouseX;
  ball.y=mouseY;
-  background(251,1,0);
+  background(252,1,0);
   display();
 
   drawSprites();
@@ -54,6 +58,13 @@ function display(){
 	rectangle1.display();
 	rectangle2.display();
 	rectangle3.display();
+	slingshot.display();
+}
+function mouseDragged(){
+    Matter.Body.setPosition(ball.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+    slingShot.fly();
 }
 
 
